8e3250e46cd215971f929169a5a6910cb65687a0
nmt1908 <beosex11223@gmail.com>
2024-05-03 14:05:41 +0700

    avt user

--- DoAnNhomC/resources/views/admin/navbar.blade.php
+++ DoAnNhomC/resources/views/admin/navbar.blade.php
@@ -38,7 +38,7 @@
 			</li>
 			<li class="nav-item dropdown">
 				<a class="nav-link p-0 pr-3" data-toggle="dropdown" href="#">
-					<img src="{{ asset('admin-acess/img/avatar5.png') }}" class='img-circle elevation-2' width="40" height="40" alt="">
+                    <img src="{{ asset('user-image/images/' . Auth::user()->img) }}" class='img-circle elevation-2' width="40" height="40" alt="">
 				</a>
 				<div class="dropdown-menu dropdown-menu-lg dropdown-menu-right p-3">
 					<h4 class="h4 mb-0"><strong>{{ Auth::user()->name }}</strong></h4>

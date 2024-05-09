eff8e60ad993b79c6c31e9f34581978706ce937c
thaitrongnghia <nghiathai.06032003@gmail.com>
2024-05-10 01:54:33 +0700

    hoàn thành xóa banner

--- DoAnNhomC/app/Http/Controllers/admin/BannerController.php
+++ DoAnNhomC/app/Http/Controllers/admin/BannerController.php
@@ -13,4 +13,17 @@ class BannerController extends Controller
         $banner = Banner::paginate(5);
         return view('admin.banner.listbanner', compact('banner'));
     }
+    public function deleteBanner(Request $request, $id)
+    {
+        $banner = Banner::findOrFail($id);
+        $imagePath = public_path('banner-image' . $banner->img_banner);
+
+    // Kiểm tra xem tệp ảnh tồn tại trước khi xóa
+    if (file_exists($imagePath)) {
+        // Xóa tệp ảnh từ thư mục
+        unlink($imagePath);
+    }
+        $banner->delete();
+        return redirect()->route('admin.listbanner')->with('success', 'Banner đã được xóa thành công');
+    }
 }
